743e6fb063ed864648c2acdcc41441dd08da1cdf
jmacias84 <jonathan.maciasb@gmail.com>
2022-06-15 13:26:41 -0500

    Admin created

--- AppCoder/views.py
+++ AppCoder/views.py
@@ -1,10 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import family
 
 def inicio(request):
     return HttpResponse("Bienvenido a mi página")
 
 def familia(request):
+    data_familia = family.objects.all()
+    print(data_familia)
     return render(request,'paginas/familia.html')
 
 # Create your views here.
